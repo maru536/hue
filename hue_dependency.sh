b52fd1541da3fdef1a51d91b260057a281f66978
maru536 <maru0218@naver.com>
2020-07-21 14:32:13 +0000

    fix dependency

--- hue_dependency.sh
+++ hue_dependency.sh
@@ -1,3 +1,4 @@
 curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -H
-sudo yum install -y ant asciidoc cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-plain gcc gcc-c++ krb5-devel libffi-devel libxml2-devel libxslt-devel make mysql mysql-devel openldap-devel python-devel sqlite-devel gmp-devel nodejs python-pip
+sudo yum install -y ant aAsciidoc cyrus-sasl-devel cyrus-sasl-gssapi cyrus-sasl-plain gcc gcc-c++ krb5-devel libffi-devel libxml2-devel libxslt-devel make mysql mysql-devel openldap-devel python-devel sqlite-devel gmp-devel nodejs python-pip
+sudo pip install --upgrade pip
 pip install -r requirements.txt
